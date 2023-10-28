b55ab4cd5eb9d87e0c5838dc5b5936498579e95a
Fatrini Nurrahmah <54012198+fnurrahmah125@users.noreply.github.com>
2023-10-28 23:58:40 +0700

    try new features

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./public/**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        isBlue: "#75CFB8",
+        tema: "rgb(var(--warna-tema) / <alpha-value>)",
+      },
+    },
   },
   plugins: [],
 };
