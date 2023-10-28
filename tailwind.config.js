0d88aa8db24fd24455574e867c210d33bbd11db9
Fatrini Nurrahmah <54012198+fnurrahmah125@users.noreply.github.com>
2023-10-28 15:54:01 +0700

    update content

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,20 @@ module.exports = {
       colors: {
         custom: "#FFC436",
       },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        wiggle: "wiggle 1s ease-in-out infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": {
+            transform: "rotate(-3deg)",
+          },
+          "50%": {
+            transform: "rotate(3deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
